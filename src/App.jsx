659b6436c2cd4cf1045b86224d5d79f26a752217
PhumuzileMoyo8 <63161922+PhumuzileMoyo8@users.noreply.github.com>
2024-04-12 00:41:24 +0000

    Build Modal and fefactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,46 @@
-import React, { Component, useState } from 'react';
-import ListCast from './components/ListCast';
+import React, { useEffect, useState } from "react";
+import ListCast from "./components/ListCast";
+import Modals from "./components/Modals";
 
 function App() {
-  const name = 'StarGazers'
+  const name = "StarGazers";
+  const [cast, setCast] = useState([]);
   let [memberInfo, setMemberInfo] = useState(null);
+
+  async function fetchCast() {
+    const response = await fetch("cast.json");
+    setCast(await response.json());
+  }
+
+  useEffect(() => {
+    fetchCast();
+  });
   return (
     <div className="container">
       <hgroup>
         <img src="images/group.svg" alt="StarGazers Group" />
         <h1>Meet the Stargazers</h1>
-        <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-        <ListCast onChoice={(info) => { setMemberInfo(info) }} />
-        {memberInfo &&
-          <article>
-            <hgroup>
-              <div style={{
-                display: "flex",
-                gap: '1rem'
-              }}>
-                <img style={{ width: '200px' }} src={`images/${memberInfo.slug}.svg`} alt={memberInfo.name} />
-                <hgroup>
-                  <h1>{memberInfo.name}</h1>
-                  <p>{memberInfo.bio}</p>
-                </hgroup>
-              </div>
-            </hgroup>
-          </article>
-        }
+        <p>
+          Members of an <b>intergalactic alliance</b> paving the way for peace
+          and benevolence among all species. They are known for their enthusiasm
+          for science, for their love of fun, and their dedication to education.
+        </p>
+        <ListCast
+          cast={cast}
+          onChoice={(info) => {
+            setMemberInfo(info);
+          }}
+        />
+        {memberInfo && (
+          <Modals
+            member={memberInfo}
+            handleClose={() => {
+              setMemberInfo(null);
+            }}
+          />
+        )}
       </hgroup>
     </div>
-  )
+  );
 }
-export default App
+export default App;
